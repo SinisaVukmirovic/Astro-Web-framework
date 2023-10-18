60bdff136729e460035119eeb46e14f0644804e2
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2023-10-18 13:25:15 +0200

    Problem with using Image astro component...

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,15 @@
 import { z, defineCollection } from 'astro:content';
 
 const postsCollection = defineCollection({
-    schema: z.object({
+    schema: ({ image }) => z.object({
         author: z.string(),
         date: z.string(),
-        image: z.string(),
+        image: image(),
         title: z.string()
     })
 })
 
 export const collection = {
-    // key name has to match folder name (posts-posts)
+    // key name has to match folder name (posts->posts)
     posts: postsCollection
 }
